5121cfe5c0ff4f3e9feaf2fb589644b03c8cdc2a
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-11-03 21:59:44 +0300

    Add project files.

---
+++ Class_1/Kullanici.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_1
+{
+    // erişim seviyesi
+    // class
+    // class adi
+
+    // Not :  Classlar soyut varlıklardır. Sadece tasarım amaçlı kullanılır
+    public class Kullanici
+    {
+        // Class Members = class üylerini ifade eder. Clsss memberlarinin default access modifiers'i (erişim seviyesi) privatedir.. (özel);
+
+        // field => class'ın alanlarıdır
+        // metot => class içerisindeki kodları yürüttümüz yapılardır.yani programladır..
+        // property 
+
+        public string adi;
+        public int yas;
+        public char cinsiye;
+    }
+}
---
+++ Class_1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Class_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Class (Sınıf) => uygulama içerisinde kendimize ait tipler oluşturabildiğimiz yapıdır..
+            // Class uygulamalardaki modüllerimizi barındandıran yapılardır...
+
+            // Class kodları kategorize eden yapıladır. Örneği ekran işlemleri Console sınıfında, dizi işlemleri Array sınıfında, dönüşüm işlemleri Convert sınıfında geliştirilmiştir..
+
+            //string[] kisi1 = { "Ekrem", "Yıldırım", "34", "E", "İstanbul" };
+            //string[] kisi2 = { "Berat", "Akyol", "23", "E", "KahramanMaraş" };
+
+            //string[][] kisiler = new string[2][];
+            //kisiler[0] = kisi1;
+            //kisiler[1] = kisi2;
+
+            // oop yaklaşımı
+
+            // classların özelliklerine nesneler üzerinden erişebiliriz.
+
+            // k değişkeni nesneyi ifade eder. Kullanıcı tipinde k nesnesi..
+
+            Kullanici k = new Kullanici();
+            k.adi = "Berat";
+            k.yas = 23;
+            k.cinsiye = 'e';
+
+            Kullanici k1 = new Kullanici();
+            k1.adi = "Umut";
+            k1.yas = 30;
+            k1.cinsiye = 'e';
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
